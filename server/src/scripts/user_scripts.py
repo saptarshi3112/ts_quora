a7ca49fc96c0b31931b7845bb2104f3829617b8e
saptarsh1996 <senguptasaptarshi11@gmail.com>
2020-12-06 14:57:05 +0530

    test scripts

--- server/src/scripts/user_scripts.py
+++ server/src/scripts/user_scripts.py
@@ -1,2 +1,25 @@
+import requests
+import json
+
+from constants import BASE_URL
+
 class User:
-    pass
+    @staticmethod
+    def login_user(email, password):
+        response = requests.post(BASE_URL+'/user/login', json.dumps({
+            "email": email,
+	        "password": password
+        }), headers={
+            'Content-Type': 'application/json'
+        })
+
+
+        parsed_data = (json.loads(response.text))
+        if 'data' in parsed_data:
+            return parsed_data['data']
+        else:
+            return '404'
+
+
+login = User.login_user("sapt@yopmail.com", "ubuntu")
+print(login)
